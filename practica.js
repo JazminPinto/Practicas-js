4c37ac02c73280466b854ccb82676a8308a3e44a
jazmin pinto <jazminpinto61@gmail.com>
2022-11-03 23:40:53 -0300

    first commit

---
+++ practica.js
@@ -0,0 +1,41 @@
+/*
+var variableGlobal = 1
+console.log(variableGlobal)
+
+let variableInterna2 = 10
+console.log('Afuera 1 >>>>', variableInterna2)
+
+function teste() {
+    let uno = 1
+    let dos = 2
+    console.log(uno + dos)
+    
+    let variableInterna = 5
+    variableInterna2 = 55
+    console.log('Adentro >>>>', variableInterna)
+}
+
+teste()
+
+console.log('Afuera 2 >>>>', variableInterna2)
+*/
+
+function suma(a, b) {
+
+    return a + b
+}
+
+let resultado = suma(1, 2)
+
+console.log(resultado)
+
+console.log(suma(1, 2))
+
+
+console.log(resultado*3)
+
+let resultado2 = resultado*3
+
+console.log(resultado2)
+
+let valor1 = 100
